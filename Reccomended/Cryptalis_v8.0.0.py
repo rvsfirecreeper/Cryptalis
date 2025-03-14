077f397371cde6d1b0f46e55f02af32c86c0e959
FireCreeper <rvsingh1872@proton.me>
2025-03-13 20:48:17 -0400

    Update and rename Cryptalis_v7.0.0.py to Cryptalis_v8.0.0.py
    Added hashing for key4, made position a key, made position changing formula more variable, improved efficiency by not using += for the string.

--- Reccomended/Cryptalis_v8.0.0.py
+++ Reccomended/Cryptalis_v8.0.0.py
@@ -15,18 +15,23 @@ character_to_index = {char: idx for idx, char in enumerate(letters)}
 def hash_key(key):
     return int(hashlib.sha256(str(key).encode()).hexdigest(), 16)
 # Decryption function
-def ecdc(text, key, key2, key3, ende):
-    position = 1
+def ecdc(text, key, key2, key3, position, ende):
     key = hash_key(key)
     key2 = hash_key(key2)
-    output = ""
+    key3=hash_key(key3)
+    output = []
     for i in range(len(text)):
         #Chacter Value
         charval=character_to_index[text[i]]
         #Formula
-        output += letters[(charval + (ende*key) - ende*(math.ceil(key2 // (i + key3))) + (ende*position)) % len(letters)]
+        outletter=(charval + (ende * key) - ende * (math.ceil(key2 // (i + key3))) + (ende * position)) % len(letters)
+        output.append(letters[outletter])
         #Change of variable
-        position = (position + i) % abs(position + key - key2)
+        if ende == 1:
+            position = (position + charval + key3) % abs(position + key - key2)
+        else:
+            position = (position + outletter + key3) % abs(position + key - key2)
+    output = "".join(output)
     return output
 
 # User Input Function
@@ -36,15 +41,18 @@ def choice():
         key = int(input("Enter key (1): "))
         key2 = int(input("Enter key (2): "))
         key3 = int(input("Enter key (3): "))
+        key4 = int(input("Enter key (4): "))
         #Validity Checks
+        if key <= key2:
+            raise ValueError("Key 1 must be greater than Key 2")
         if key > key_max:
             raise ValueError("Key 1 must be less than 10^40")
         if key2 > key_max:
             raise ValueError("Key 2 must be less than 10^40")
-        if key <= key2:
-            raise ValueError("Key 1 must be greater than Key 2")
         if key3 > key_max:
             raise ValueError(f"Key 3 must be less than 10^40")
+        if key4 > key_max:
+            raise ValueError(f"Key 4 must be less than 10^40")
         if key < 0 or key2 < 0 or key3 < 0:
             raise ValueError("Keys must be positive")
     #Error Handling
@@ -56,9 +64,9 @@ def choice():
     text = input("Enter text to en/decrypt: ")
     start = time.time() 
     if user_choice == "ec":
-        return f"It took {time.time()-start} to encrypt. the text is:{ecdc(text, key, key2, key3,1)}"
+        return f"It took {time.time()-start} to encrypt. the text is:{ecdc(text, key, key2, key3, key4,1)}"
     elif user_choice == "dc":
-        return f"It took {round(time.time()-start)} seconds to decrypt. the text is:{ecdc(text, key, key2, key3, -1)}"
+        return f"It took {round(time.time()-start)} seconds to decrypt. the text is:{ecdc(text, key, key2, key3, key4, -1)}"
     else:
         print("Invalid choice")
         return choice()
