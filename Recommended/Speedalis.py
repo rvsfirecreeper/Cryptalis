852844aac4ea7d90fc5911d6cbc8df36d8101f74
FireCreeper <rvsingh1872@proton.me>
2025-03-28 19:42:37 -0400

    Update Speedalis.py

--- Recommended/Speedalis.py
+++ Recommended/Speedalis.py
@@ -2,23 +2,25 @@
 import hashlib
 import math
 import time
+import argon2
 # Maxes for key 1, 2, and 3
-key_max = 10**40
 # Characters
-letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v","w", "x", "y", "z", 
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v","w", "x", "y", "z",
            "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
-           "!", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", " ", 
+           "!", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", " ",
            ",", ".", "?",":", "'", "\"", "-", "_", "(", ")", "[", "]", "{", "}", "/", "\\", "*", "&", "^", "%", "$", "#", "@", "=", "+", "`", "~", "<", ">", "↑", "↓","→","←"]
 # Mapping Dictionary
 character_to_index = {char: idx for idx, char in enumerate(letters)}
 # Hashing function
 def hash_key(key):
-    return int(hashlib.sha256(str(key).encode()).hexdigest(), 16)
-# Decryption function
+    salt = hashlib.sha256(str(key).encode()).digest()[:16]
+    return int.from_bytes(argon2.low_level.hash_secret_raw(secret = key.encode(),salt = salt, type = argon2.Type.ID, time_cost = 4, memory_cost = 2**16, parallelism = 4, hash_len = 32))
+# En/Decryption function
 def ecdc(text, key, key2, key3, position, ende):
     key = hash_key(key)
     key2 = hash_key(key2)
-    key3=hash_key(key3)
+    key3 = hash_key(key3)
+    position = hash_key(position)
     # Precomputed Values
     pc1 = (ende * key)
     pc2 = key - key2
@@ -42,23 +44,15 @@ def ecdc(text, key, key2, key3, position, ende):
 def choice():
     try:
         #Input
-        key = int(input("Enter key (1): "))
-        key2 = int(input("Enter key (2): "))
-        key3 = int(input("Enter key (3): "))
-        key4 = int(input("Enter key (4): "))
-        #Validity Checks
-        if key <= key2:
-            raise ValueError("Key 1 must be greater than Key 2")
-        if key > key_max:
-            raise ValueError("Key 1 must be less than 10^40")
-        if key2 > key_max:
-            raise ValueError("Key 2 must be less than 10^40")
-        if key3 > key_max:
-            raise ValueError(f"Key 3 must be less than 10^40")
-        if key4 > key_max:
-            raise ValueError(f"Key 4 must be less than 10^40")
-        if key < 0 or key2 < 0 or key3 < 0:
-            raise ValueError("Keys must be positive")
+        key = (input("Enter key (1): "))
+        key2 = (input("Enter key (2): "))
+        key3 = (input("Enter key (3): "))
+        key4 = input("Enter key (4): ")
+        #Catch errors
+        if key == "" or key2 == "" or key3 == "" or key4 == "":
+            raise ValueError("Key cannot be empty")
+        if key == key2:
+            raise ValueError("Key 1 and Key 2 cannot be the same, this will cause ZeroDivisionError")
     #Error Handling
     except ValueError as e:
         print(f"Error: {e}. Please enter a number or ensure Key 1 > Key 2. Make sure key 1 and 2 are under 10**40 and key 3 is under 5000")
@@ -66,7 +60,7 @@ def choice():
 
     user_choice = input("Encrypt (ec) or Decrypt (dc)? ")
     text = input("Enter text to en/decrypt: ")
-    start = time.time() 
+    start = time.time()
     if user_choice == "ec":
         return f"It took {time.time()-start} to encrypt. the text is:{ecdc(text, key, key2, key3, key4,1)}"
     elif user_choice == "dc":
